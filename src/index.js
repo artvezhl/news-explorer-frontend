83000ffad23fe8ae4123e7aa32df724f0b84b498
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-10-07 17:36:12 +0300

    configure webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 // TODO test "start" script in package.json и проверить сборки в проекте
 // TODO доработать readme.md
+// TODO отработать по поддерживаемым браузерам и возможному созданию нового отдельного файла с браузерами
 
-console.log('Hello, Webpack!!!');
+import "./style.css";
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
