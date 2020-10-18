95230fc84c0de5c3a2d285eb0780ac033a97f53c
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-10-19 00:30:07 +0300

    add 768px header and cover blocks

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const header = document.querySelector('.header');
 const menuButton = document.querySelector('.header__mobile-menu');
 const closeButton = document.querySelector('.popup__mobile-menu');
 const popup = document.querySelector('.popup');
+const authButton = document.querySelector('.popup__button');
+const popupAuth = document.querySelector('.popup-auth');
 
 const scrollY = document.body.style.top;
 document.body.style.position = '';
@@ -24,8 +26,17 @@ menuButton.addEventListener('click', (event) => {
 });
 
 closeButton.addEventListener('click', () => {
-  header.classList.toggle('header_type_popup-opened');
-  popup.classList.toggle('popup_is-opened');
+  header.classList.remove('header_type_popup-opened');
+  popup.classList.remove('popup_is-opened');
+  popupAuth.classList.remove('popup_is-opened');
+  menuButton.classList.remove('header__mobile-menu_type_opened');
   document.body.style.position = '';
   document.body.style.top = '';
+});
+
+authButton.addEventListener('click', () => {
+  popupAuth.classList.toggle('popup_is-opened');
+  popup.classList.toggle('popup_is-opened');
+  header.classList.add('header_type_popup-opened');
+  menuButton.classList.add('header__mobile-menu_type_opened');
 });
