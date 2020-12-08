b23050338da90add2d635d046f4afecf5e66874f
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-12-08 17:38:17 +0300

    fix newsConfig: add today and 7 days ago date

--- src/js/configs/newsConfig.js
+++ src/js/configs/newsConfig.js
@@ -1,11 +1,14 @@
-// TODO renew date in URL
+const today = new Date().toISOString().substring(0,10);
+const oneWeekAgo = new Date(new Date() - ((24 * 60 * 60 * 1000) * 7)).toISOString().substring(0,10);
+
 const newsConfig = {
   url: (string) => {
     return `https://newsapi.org/v2/everything?` +
       `q=${string}&` +
-      `from=2020-11-10&to=2020-12-03&` +
+      `from=${oneWeekAgo}&to=${today}&` +
       `sortBy=relevancy&` +
-      `apiKey=d4a483a63e6f45ea9d32eefd97080f26`
+      `apiKey=d4a483a63e6f45ea9d32eefd97080f26&` +
+      `pageSize=100`
   },
 }
 
