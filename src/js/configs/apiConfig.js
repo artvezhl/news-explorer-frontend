6aeb517243c5c44156610aad0af887fd258ac4e3
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-20 01:34:02 +0300

    change config

--- src/js/configs/apiConfig.js
+++ src/js/configs/apiConfig.js
@@ -1,5 +1,7 @@
 const apiConfig = {
-  baseUrl: 'http://localhost:3000',
+  baseUrl: (process.env.NODE_ENV === 'development')
+    ? 'http://localhost:3000'
+    : 'http://api.diploma-web.tk',
   headers: {
     'Content-Type': 'application/json',
     'authorization': `${localStorage.getItem('token')}`,
