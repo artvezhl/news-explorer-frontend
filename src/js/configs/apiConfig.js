b50bf93595ef4d302fe48f095248e82efe599568
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-23 00:46:09 +0300

    chenge api config

--- src/js/configs/apiConfig.js
+++ src/js/configs/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
   baseUrl: (process.env.NODE_ENV === 'development')
     ? 'http://localhost:3000'
-    : 'http://api.diploma-web.tk',
+    : 'https://www.api.diploma-web.tk',
   headers: {
     'Content-Type': 'application/json',
     'authorization': `${localStorage.getItem('token')}`,
